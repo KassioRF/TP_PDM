c1c6d4ade5cc8527a27c35a3b64fb5784573d0de
Kassio R Ferreira <kassio_rf@hotmail.com>
2022-09-10 18:25:42 -0300

    Home records list alignment

---
+++ gkfin/lib/utils/styles.dart
@@ -0,0 +1,3 @@
+// ignore_for_file: avoid_types_as_parameter_names, non_constant_identifier_names, unused_element
+
+// import 'package:flutter/material.dart';
--- gkfin/lib/views/home.dart
+++ gkfin/lib/views/home.dart
@@ -7,6 +7,18 @@ import 'package:flutter/material.dart';
 import '../models/records.dart';
 import '../data/dummy_data.dart';
 
+// default box decoration 
+final _boxDecoration =  BoxDecoration(
+  borderRadius: BorderRadius.circular(2),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withOpacity(.1),
+        offset: Offset(0,3),
+      )
+    ]
+);
+
+
 class HomeView extends StatefulWidget {
   const HomeView({super.key, required this.title});
 
@@ -23,6 +35,7 @@ class _HomeView extends State<HomeView> with SingleTickerProviderStateMixin {
   late TabController _tabController;
   final List<Record> _mockData = DUMMY_RECORDS;
   
+
   @override
   void initState(){
     super.initState();
@@ -31,6 +44,10 @@ class _HomeView extends State<HomeView> with SingleTickerProviderStateMixin {
   // ignore: empty_constructor_bodies
   @override
   Widget build(BuildContext context) {
+  //DATA FOR WIDGET REGISTER ITEMS
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 60,
@@ -39,11 +56,12 @@ class _HomeView extends State<HomeView> with SingleTickerProviderStateMixin {
         title: Text('Hello, User'),
         actions: [
           //@TODO ADD BTN EVENT
-          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert),)
-        
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert),)        
         ],
         bottom: TabBar(
           controller: _tabController,
+          // unselectedLabelColor: Theme.of(context).backgroundColor,
+          // indicator: BoxDecoration(color: Theme.of(context).backgroundColor),
           tabs: <Widget> [
             Tab(icon: Icon(Icons.currency_exchange_rounded)),
             Tab(icon: Icon(Icons.stacked_bar_chart, size: 32.0)),
@@ -90,59 +108,99 @@ class _HomeView extends State<HomeView> with SingleTickerProviderStateMixin {
               ),
             ),
               // Data slider
-            Divider(indent: 15, endIndent: 15,),
+            // Divider(indent: 15, endIndent: 15,),
             Expanded(
               flex: 1,
-              child: Row(                
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  //@TODO ADD BTN EVENT
-                  IconButton(onPressed: (){}, icon: Icon(Icons.keyboard_arrow_left)),
-                  Text(_currMonth,
-                    style: TextStyle(
-                      fontStyle: FontStyle.italic,
-                      fontSize: 12,
+              child: Container(                
+                // color: Theme.of(context).backgroundColor,
+                // @TODO Include box decoration
+                decoration: _boxDecoration,
+                child: Row(                
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    //@TODO ADD BTN EVENT
+                    IconButton(onPressed: (){}, icon: Icon(Icons.keyboard_arrow_left)),
+                    Text(_currMonth,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontSize: 12,
+                      ),
                     ),
-                  ),
-                  //@TODO ADD BTN EVENT
-                  IconButton(onPressed: (){}, icon: Icon(Icons.keyboard_arrow_right)),
-                ],
+                    //@TODO ADD BTN EVENT
+                    IconButton(onPressed: (){}, icon: Icon(Icons.keyboard_arrow_right)),
+                  ],
+                ),
               ),
             ),
-            Divider(indent: 15, endIndent: 15, height: 15,),
+            // Divider(height: 15,),
             
             //Records Filters
             Expanded(
               flex: 1,
+        
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: <Widget>[
                   Text('Lançamaentos'),
                   IconButton(
-                    onPressed: (){},                    
-                    //@TODO ADD BTN EVENT
-                    icon: IconButton(onPressed: (){}, icon: Icon(Icons.filter_list),)
-                  )
+                  onPressed: (){},                    
+                  //@TODO ADD BTN EVENT
+                  icon: IconButton(onPressed: (){}, icon: Icon(Icons.filter_list),)
+                  ),
                 ]
               ),
             ),
             
             // ListView dentro de Cloumn: https://www.youtube.com/watch?v=Gylc2SiLxmE
-            Divider(indent: 15, endIndent: 15, height: 15,),
+            Divider(height: 15,),
             Expanded(
-              flex: 9,
-              child: ListView.builder(
-                // shrinkWrap: true,
-                itemCount: _mockData.length*3,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text('Item $index')
-                    // child: Column(children: [
-                    //   Text("item $index"),Text("item $index"),Text("item $index"),Text("item $index")
-                    // ]),
-                  );
-                },            
-              ),
+              flex: 8,
+              
+              child: Container(
+                decoration: _boxDecoration,
+               
+                child: ListView.builder(
+                  // shrinkWrap: true,
+                  itemCount: _mockData.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    //@TODO Build an Widget
+                    return SizedBox(
+                        width: width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            // Text(_mockData[index].type),
+                            // Text(_mockData[index].desc),
+                            // Text('${_mockData[index].value}'),
+                            
+                            Expanded(
+                              flex: 1,
+                              child: Text(_mockData[index].type),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(_mockData[index].desc),
+                            ),
+                            Expanded(
+                              flex:1,
+                              child: Text('${_mockData[index].value}'),
+                            ),
+                          ],
+                          
+                        ),
+                    );
+                    // return ListTile(
+                    //   title: Text('Item $index')
+                    //   // child: Column(children: [
+                    //   //   Text("item $index"),Text("item $index"),Text("item $index"),Text("item $index")
+                    //   // ]),
+                    
+                    // );
+                  },            
+                ),
+              )
             ),
           ],
 
